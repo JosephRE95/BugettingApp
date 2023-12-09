4062b59570611e51d2f5c3355c7916dbbd1ad0c5
Joseph Ellis <josephellis@Josephs-MacBook-Air.local>
2023-12-09 13:50:17 -0600

    reset the process to fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import  { createBrowserRouter, RouterProvider, useLoaderData } from "react-router-dom";
-import {fetchData} from './helpers'
 
-// Routes
-// import Dashboard, { dashboardLoader } from "./assets/pages/Dashboard";
-import Error from "../src/assets/pages/Error";
-import About from "./assets/pages/About";
-localStorage.setItem('userName', 'joseph')
-export function dashboardLoader() {
-  const userName = fetchData("userName");
-  return  userName
-}
 
-export function Dashboard(){
-  const  userName  = useLoaderData()
-console.log('userName')
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  )
-}
+
+
 
 const router = createBrowserRouter([
   {
--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -1,21 +1,7 @@
-// rrd imports
-// import { useLoaderData } from "react-router-dom";
-
-// //  helper functions
-// import { fetchData } from '../../helpers';
-
-// // loader
-// export function dashboardLoader() {
-//   const userName = fetchData("userName");
-//   return  userName 
-// }
-
 const Dashboard = () => {
-  const { userName } = useLoaderData()
 
   return (
     <div>
-      <h1>{userName}</h1>
       Dashboard
     </div>
   )
