6d4d476647a3d3e86e814986117c5201ae15d4d7
Joseph Ellis <josephellis95@gmail.com>
2023-12-12 19:20:24 -0600

    test3

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,13 @@
+// import React from 'react'
+
+// export const Intro = () => {
+//   return (
+//     <div>Intro</div>
+//   )
+// }
+
+// export default Intro
+
 import React from 'react'
 
 export const Intro = () => {
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { fetchData } from '../helpers'
-import { useLoaderData } from 'react-router-dom'
-import Intro from '../components/Intro'
-export function dashBoardLoader(){
-    const userName = fetchData("userName")
-    return { userName }
-}
+import React from "react";
+import { fetchData } from "../helpers";
+import { useLoaderData } from "react-router-dom";
+import Intro from "../components/Intro";
+// export function dashBoardLoader(){
+//     const userName = fetchData("userName")
+//     return { userName }
+// }
 
- const Dashboard = () => {
-   const { userName } = useLoaderData()
-
-  return (
-    <>
-         {userName ? (<p>{ userName }</p>) : <Intro />}
-        
-        </> 
-  )
+export function dashBoardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
 }
-export default Dashboard
+const Dashboard = () => {
+  const { userName } = useLoaderData();
+
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+};
+export default Dashboard;
