984131e7504994c27248c8261e947306e29318c3
Joseph Ellis <josephellis95@gmail.com>
2023-12-19 19:32:47 -0600

    intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,18 +1,33 @@
-// import React from 'react'
-
-// export const Intro = () => {
-//   return (
-//     <div>Intro</div>
-//   )
-// }
-
-// export default Intro
-
 import React from 'react'
 
 export const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className='intro'>
+    <div>
+      <h1>
+        Take control of
+         <span className='accent'>Your money
+
+        </span>
+      </h1>
+      <p>
+        personal budgeting is the secret to 
+        finacial freedom. Start your journey today.
+      </p>
+      <Form method="post">
+        <input
+         type="text"
+         name='userName'
+         required
+         placeholder='What is your name?'
+         aria-label='Name'
+         autoComplete='given-name'
+          
+        />
+         
+      </Form>
+    </div>
+    </div>
   )
 }
 
