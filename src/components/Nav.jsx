d5e3b6ee7a9651ba8f82ab1d6ed34d384fc9cec2
Joseph Ellis <josephellis95@gmail.com>
2024-12-05 21:10:13 -0600

    nav updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,8 @@
 // rrd imports
-import { Form, NavLink } from "react-router-dom"
+import { Form, NavLink } from "react-router-dom";
 
 // library
-import { TrashIcon } from '@heroicons/react/24/solid'
+import { TrashIcon } from '@heroicons/react/24/solid';
 
 // assets
 import logomark from "../assets/logo.svg";
@@ -10,33 +10,42 @@ import logomark from "../assets/logo.svg";
 const Nav = ({ userName }) => {
   return (
     <nav>
+      {/* Home Link */}
       <NavLink
         to="/"
         aria-label="Go to home"
       >
-        <img src={logomark} alt="" height={30} />
+        <img src={logomark} alt="HomeBudget Logo" height={30} />
         <span>HomeBudget</span>
       </NavLink>
-      {
-        userName && (
-          <Form
-            method="post"
-            action="logout"
-            onSubmit={(event) => {
-              if (!confirm("Delete user and all data?")) {
-                event.preventDefault()
-              }
-            }}
-          >
-            <button type="submit" className="btn btn--warning">
-              <span>Delete User</span>
-              <TrashIcon width={20} />
-            </button>
 
-          </Form>
-        )
-      }
+      {/* Game Link */}
+      <NavLink
+        to="/game"
+        aria-label="Play Rock-Paper-Scissors"
+      >
+        <span>Play Game</span>
+      </NavLink>
+
+      {/* Delete User Action */}
+      {userName && (
+        <Form
+          method="post"
+          action="logout"
+          onSubmit={(event) => {
+            if (!confirm("Delete user and all data?")) {
+              event.preventDefault();
+            }
+          }}
+        >
+          <button type="submit" className="btn btn--warning">
+            <span>Delete User</span>
+            <TrashIcon width={20} />
+          </button>
+        </Form>
+      )}
     </nav>
-  )
-}
-export default Nav
+  );
+};
+
+export default Nav;
